b2e31c32feb7aa0d97002a16e25ea0807d74af0a
Cain Voong <cainux@gmail.com>
2026-01-10 13:50:45 +0000

    Commenting out ensure_installed - will rely on manually installing

--- nvim/.config/nvim/lua/cain/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/cain/plugins/lsp/mason.lua
@@ -1,30 +1,30 @@
 return {
     "mason-org/mason-lspconfig.nvim",
-    opts = {
-        ensure_installed = {
-            "lua_ls",
-
-            -- my day job
-            "csharp_ls",
-            "terraformls",
-            "tflint",
-            "dockerls",
-            "docker_compose_language_service",
-            "bashls",
-            "jsonls",
-            "protols",
-
-            -- bit of fun
-            "svelte",
-            "ts_ls",
-            "cssls",
-            "rust_analyzer",
-            "sqlls",
-            "eslint",
-            "powershell_es",
-            "html",
-        }
-    },
+    -- opts = {
+    --     ensure_installed = {
+    --         "lua_ls",
+    --
+    --         -- my day job
+    --         "csharp_ls",
+    --         "terraformls",
+    --         "tflint",
+    --         "dockerls",
+    --         "docker_compose_language_service",
+    --         "bashls",
+    --         "jsonls",
+    --         "protols",
+    --
+    --         -- bit of fun
+    --         "svelte",
+    --         "ts_ls",
+    --         "cssls",
+    --         "rust_analyzer",
+    --         "sqlls",
+    --         "eslint",
+    --         "powershell_es",
+    --         "html",
+    --     }
+    -- },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
         "neovim/nvim-lspconfig",
